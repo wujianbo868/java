0bc875edb4199f081c3c252d1aee5f6922913f1d
wujianbo <wujianbo@youzan.com>
2018-04-25 20:44:46 +0800

    initial

---
+++ java-demo/src/test/java/com/wujianbo/lamda/LamdaTest.java
@@ -0,0 +1,82 @@
+package com.wujianbo.lamda;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * Created by wujianbo on 2018/4/24.
+ */
+public class LamdaTest {
+
+    @Test
+    public void testThread(){
+        new Thread( () -> System.out.println("In Java8, Lambda expression rocks !!") ).start();
+    }
+
+    @Test
+    public void testIterator(){
+        List features = Arrays.asList("Lambdas", "Default Method", "Stream API", "Date and Time API");
+        features.forEach(n -> System.out.println(n));
+
+        // 使用Java 8的方法引用更方便，方法引用由::双冒号操作符标示，
+        // 看起来像C++的作用域解析运算符
+        features.forEach(System.out::println);
+    }
+
+    @Test
+    public void testPredicate(){
+        List<String> languages = Arrays.asList("Java", "Scala", "C++", "Haskell", "Lisp");
+
+        // 甚至可以用and()、or()和xor()逻辑函数来合并Predicate，
+        // 例如要找到所有以J开始，长度为四个字母的名字，你可以合并两个Predicate并传入
+        Predicate<String> startsWithJ = (n) -> n.startsWith("J");
+        Predicate<String> fourLetterLong = (n) -> n.length() == 4;
+        languages.stream()
+                .filter(startsWithJ.and(fourLetterLong))
+                .forEach((n) -> System.out.print("nName, which starts with 'J' and four letter long is : " + n));
+    }
+
+    @Test
+    public void testMap(){
+        List costBeforeTax = Arrays.asList(100, 200, 300, 400, 500);
+        costBeforeTax.stream().map((cost) -> ((int)cost+(int)cost)).forEach(System.out::println);
+    }
+
+    @Test
+    public void testReduce(){
+        List costBeforeTax = Arrays.asList(100, 200, 300, 400, 500);
+        int bill = (int) costBeforeTax.stream().map((cost) -> ((int)cost+(int)cost))
+                .reduce((sum, cost) -> (int)sum + (int)cost).get();
+        System.out.println("Total : " + bill);
+    }
+
+    @Test
+    public void testFilter(){
+        List<String> features = Arrays.asList("Lambdas", "Default Method", "Stream API", "Date and Time API");
+        List<String> filtered = features.stream().filter(x -> x.length()> 2).collect(Collectors.toList());
+        System.out.printf("Original List : %s, filtered list : %s %n", features, filtered);
+    }
+
+    @Test
+    public void testDistinct(){
+        List<Integer> numbers = Arrays.asList(9, 10, 3, 4, 7, 3, 4);
+        List<Integer> distinct = numbers.stream().map( i -> i*i).distinct().collect(Collectors.toList());
+        System.out.printf("Original List : %s,  Square Without duplicates : %s %n", numbers, distinct);
+    }
+
+    @Test
+    public void testSummary(){
+        List<Integer> primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+        IntSummaryStatistics stats = primes.stream().mapToInt((x) -> x).summaryStatistics();
+        System.out.println("Highest prime number in List : " + stats.getMax());
+        System.out.println("Lowest prime number in List : " + stats.getMin());
+        System.out.println("Sum of all prime numbers : " + stats.getSum());
+        System.out.println("Average of all prime numbers : " + stats.getAverage());
+    }
+
+}
