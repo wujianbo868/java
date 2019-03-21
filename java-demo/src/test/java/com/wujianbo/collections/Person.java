dc83040179fd1c0f5011d5afa901438fa346f4fd
wujianbo <349561030@qq.com>
2019-03-21 20:27:01 +0800

    测试比对接口

--- java-demo/src/test/java/com/wujianbo/collections/Person.java
+++ java-demo/src/test/java/com/wujianbo/collections/Person.java
@@ -8,7 +8,13 @@ import lombok.experimental.Accessors;
  */
 @Data
 @Accessors(chain = true)
-public class Person {
+public class Person implements Comparable<Person>{
     private String name;
     private Integer age;
+    private Integer realAge;
+
+    @Override
+    public int compareTo(Person o) {
+        return (this.age - this.realAge) - (o.getAge() - o.getRealAge());
+    }
 }
--- java-demo/src/test/java/com/wujianbo/collections/SortTest.java
+++ java-demo/src/test/java/com/wujianbo/collections/SortTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -14,12 +15,23 @@ public class SortTest {
 
     @Test
     public void testSort(){
-        Person person1 = new Person().setName("wujianbo").setAge(22);
-        Person person2 = new Person().setName("zhangyuan").setAge(21);
+        Person person1 = new Person().setName("wujianbo").setAge(22).setRealAge(1);
+        Person person2 = new Person().setName("zhangyuan").setAge(21).setRealAge(20);
         List<Person> people = Arrays.asList(person1,person2);
         //这个是降序的，升序去掉reversed
         people.sort(Comparator.comparing(Person::getAge).reversed());
         people.forEach(n->System.out.print(n.getName()));
         Assert.assertTrue(people.get(0).getName().equals("wujianbo"));
+        Assert.assertTrue(person1.compareTo(person2) > 0);
+        Collections.sort(people);
+        Assert.assertEquals("zhangyuan",people.get(0).getName());
     }
+
+    @Test
+    public void testArraysSort(){
+        int[] arrays = {4,2,3,1};
+        Arrays.sort(arrays);
+        Assert.assertEquals(1,arrays[0]);
+    }
+
 }
